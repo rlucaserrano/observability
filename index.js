85fafe9ff553bef500e649b40db6f878406f57be
rlucaserrano <36525469+rlucaserrano@users.noreply.github.com>
2023-04-20 20:18:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-const { Resource } = require("@opentelemetry/resources");
-const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
-const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { trace } = require("@opentelemetry/api");
-//Instrumentations
-const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
-const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-//Exporter
-module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
-   const provider = new NodeTracerProvider({
-       resource: new Resource({
-           [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-       }),
+const tracer = require("./tracing")("todo-service");
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+app.use(express.json());
+const port = 3000;
+let db;
+const startServer = async () => {
+   const client = await MongoClient.connect("mongodb://localhost:27017/");
+   db = client.db("todo");
+   await db.collection("todos").insertMany([
+       { id: "1", title: "Buy groceries" },
+       { id: "2", title: "Install Aspecto" },
+       { id: "3", title: "buy my own name domain" },
+   ]);
+   app.listen(port, () => {
+       console.log(`Example app listening on port ${port}`);
    });
-   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-   provider.register();
-   registerInstrumentations({
-       instrumentations: [
-           new HttpInstrumentation(),
-           new ExpressInstrumentation(),
-           new MongoDBInstrumentation(),
-       ],
-       tracerProvider: provider,
-   });
-   return trace.getTracer(serviceName);
 };
+startServer();
+app.get("/todo", async (req, res) => {
+   const todos = await db.collection("todos").find({}).toArray();
+   res.send(todos);
+});
+app.get("/todo/:id", async (req, res) => {
+   const todo = await db.collection("todos").findOne({ id: req.params.id });
+   res.send(todo);
+});
